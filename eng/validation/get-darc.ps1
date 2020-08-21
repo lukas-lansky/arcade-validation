104414bdf8a300ba94b243bb3d26ed9af5f620ca
Epsitha Ananth <epananth@microsoft.com>
2020-08-21 16:07:44 -0700

    test

--- eng/validation/get-darc.ps1
+++ eng/validation/get-darc.ps1
@@ -1,5 +1,5 @@
 $path = "$PSScriptRoot\darc\$(New-Guid)"
 
-& $PSScriptRoot\..\common\darc-init.ps1 -toolpath $path | Out-Host
+& $PSScriptRoot\..\common\darc-init.ps1 -darcVersion "1.1.0-beta.20418.1" -toolpath $path | Out-Host
 
 return "$path\darc.exe"
